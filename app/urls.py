fb4a5460507046e0e31d548f4ba00eefac4de458
Administrator <1101066558@qq.com>
2016-12-09 17:48:07 +0800

    idc features

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'index/', views.index),
+    url(r'^index/$', views.index),
+    url(r'^idc/$', views.idc),
+    url(r'^get_add_idc_page/$', views.get_add_idc_page),
+    url(r'^add_idc/$', views.add_idc)
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render, render_to_response
+# -*- coding:utf-8 -*-
+from django.shortcuts import render, render_to_response, redirect
+from app.models import *
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 # Create your views here.
-
 def index(request):
-    return render_to_response("index.html",{})
+    return render_to_response("index.html", {})
+
+##### idc ###########
+def idc(request):
+    idc_list = Idc.objects.all()
+    return render_to_response("idc.html", locals())
+
+
+def get_add_idc_page(request):
+    return render_to_response("add_idc.html", {})
+
+
+def add_idc(request):
+    idc_name = request.GET.get("idc_name")
+    idc_remark = request.GET.get("idc_remark")
+
+    idc = Idc()
+    idc.idc_name = str(idc_name)
+    idc.remark = str(idc_remark)
+    idc.save()
+    return redirect("/app/idc/")
